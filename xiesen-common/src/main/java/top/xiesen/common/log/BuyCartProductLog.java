b1b1c43cbadc709eb567820bdb19f2242c45766e
xiesen110 <xiesen310@qq.com>
2020-02-15 15:52:45 +0800

    <dev> 用户画像之公共服务

---
+++ xiesen-common/src/main/java/top/xiesen/common/log/BuyCartProductLog.java
@@ -0,0 +1,52 @@
+package top.xiesen.common.log;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Description 购物车行为日志
+ * @className top.xiesen.analy.log.CollectProductLog
+ * @Author 谢森
+ * @Email xiesen310@163.com
+ * @Date 2020/2/15 13:49
+ */
+@Data
+public class BuyCartProductLog implements Serializable {
+    /**
+     * 商品 id
+     */
+    private int productId;
+
+    /**
+     * 商品类型 id
+     */
+    private int productTypeId;
+
+    /**
+     * 操作时间
+     */
+    private String operationTime;
+
+    /**
+     * 操作类型
+     * 0 加入
+     * 1 删除
+     */
+    private int operationType;
+
+    /**
+     * 用户 id
+     */
+    private int userId;
+
+    /**
+     * 终端类型:0、pc端；1、移动端；2、小程序
+     */
+    private int useType;
+
+    /**
+     * 用户ip
+     */
+    private String ip;
+}
---
+++ xiesen-common/src/main/java/top/xiesen/common/log/ScanProductLog.java
@@ -0,0 +1,51 @@
+package top.xiesen.common.log;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Description 浏览商品日志
+ * @className top.xiesen.analy.log.ScanProductLog
+ * @Author 谢森
+ * @Email xiesen310@163.com
+ * @Date 2020/2/15 13:43
+ */
+@Data
+public class ScanProductLog implements Serializable {
+    /**
+     * 商品 id
+     */
+    private int productId;
+
+    /**
+     * 商品类型 id
+     */
+    private int productTypeId;
+
+    /**
+     * 浏览时间
+     */
+    private String scanTime;
+
+    /**
+     * 停留时间
+     */
+    private String stayTime;
+
+    /**
+     * 用户 id
+     */
+    private int userId;
+
+    /**
+     * 终端类型:0、pc端；1、移动端；2、小程序
+     */
+    private int useType;
+
+    /**
+     * 用户ip
+     */
+    private String ip;
+
+}
